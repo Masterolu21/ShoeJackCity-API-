04d639632a61f54a79ad8bd230afb65ee119ae0c
Miguel Diaz <diazmiiguel@gmail.com>
2018-09-13 17:19:30 -0400

    Removes floats from factory

--- test/support/factory.ex
+++ test/support/factory.ex
@@ -8,10 +8,10 @@ defmodule Sjc.Factory do
   def player_factory do
     %{
       id: sequence(:id, &(&1 + 1)),
-      health_points: 50.0,
-      shield_points: 40.0,
-      accuracy: sequence(:accuracy, &(&1 + 17.2)),
-      luck: sequence(:luck, &(&1 + 8.6))
+      health_points: 50,
+      shield_points: 0,
+      accuracy: sequence(:accuracy, &(&1 + 17)),
+      luck: sequence(:luck, &(&1 + 8))
     }
   end
 end
