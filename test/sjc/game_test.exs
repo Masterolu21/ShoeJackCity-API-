7a602d453bd04b346f9c77ee46b010810e0a4b26
Miguel Diaz <diazmiiguel@gmail.com>
2018-10-25 14:42:59 -0400

    Remade game tests

--- test/sjc/game_test.exs
+++ test/sjc/game_test.exs
@@ -10,293 +10,272 @@ defmodule Sjc.GameTest do
   alias Sjc.Game.Inventory
   alias Sjc.Game
 
+  setup_all do
+    {:ok, pid} = GameSupervisor.start_child("game")
+
+    {:ok, pid: pid}
+  end
+
   setup do
     mock_global(fn env -> env end)
 
     player_attributes = build(:player)
 
+    Game.clean_game("game")
+
     {:ok, player_attrs: player_attributes}
   end
 
-  describe "game" do
-    test "adds another round to the game" do
-      GameSupervisor.start_child("game_1")
+  test "adds another round to the game" do
+    # True by default, changes it to false so it doesn't call next round automatically.
+    Game.shift_automatically("game")
 
-      # True by default, changes it to false so it doesn't call next round automatically.
-      Game.shift_automatically("game_1")
+    Game.next_round("game")
 
-      Game.next_round("game_1")
+    assert Game.state("game").round.number == 2
+  end
 
-      assert Game.state("game_1").round.number == 2
-    end
+  test "creates a player struct correctly", %{player_attrs: attributes} do
+    assert {:ok, :added} = Game.add_player("game", attributes)
+    assert length(Game.state("game").players) == 1
+  end
 
-    test "creates a player struct correctly", %{player_attrs: attributes} do
-      GameSupervisor.start_child("game_1")
+  test "returns {:error, already added} when player is a duplicate", %{player_attrs: attributes} do
+    assert {:ok, :added} = Game.add_player("game", attributes)
+    assert {:error, :already_added} = Game.add_player("game", attributes)
+  end
 
-      assert {:ok, :added} = Game.add_player("game_1", attributes)
-      assert length(Game.state("game_1").players) == 1
-    end
+  test "removes player from game by identifier", %{player_attrs: attributes} do
+    {:ok, :added} = Game.add_player("game", attributes)
 
-    test "returns {:error, already added} when player is a duplicate", %{player_attrs: attributes} do
-      GameSupervisor.start_child("game_5")
+    players_fn = fn -> length(Game.state("game").players) end
 
-      assert {:ok, :added} = Game.add_player("game_5", attributes)
-      assert {:error, :already_added} = Game.add_player("game_5", attributes)
-    end
+    # Player length at this point
+    assert players_fn.() == 1
 
-    test "removes player from game by identifier", %{player_attrs: attributes} do
-      {:ok, _} = GameSupervisor.start_child("game_7")
-      {:ok, :added} = Game.add_player("game_7", attributes)
+    Game.remove_player("game", attributes.id)
 
-      players_fn = fn -> length(Game.state("game_7").players) end
+    assert players_fn.() == 0
+  end
 
-      # Player length at this point
-      assert players_fn.() == 1
+  test "automatically shifts round when a specified amount of time has passed" do
+    assert Game.state("game").round.number >= 1
+  end
 
-      Game.remove_player("game_7", attributes.id)
+  test "shows the elapsed time" do
+    assert Timex.is_valid?(Game.state("game").time_of_round)
+  end
 
-      assert players_fn.() == 0
-    end
+  test "doesn't add existing players when adding from a list", %{player_attrs: player1} do
+    players = build_list(3, :player)
 
-    test "automatically shifts round when a specified amount of time has passed" do
-      {:ok, _} = GameSupervisor.start_child("game_8")
+    {:ok, :added} = Game.add_player("game", player1)
 
-      assert Game.state("game_8").round.number >= 1
-    end
+    # We just add the whole list since so we don't loop through all of them,
+    # otherwise we could have sticked with the previous solution of looping
+    # through each player's attributes.
+    {:ok, :added} = Game.add_player("game", players)
 
-    test "shows the elapsed time" do
-      {:ok, _} = GameSupervisor.start_child("game_9")
+    # We should only have 3 players since id 1 was already added
+    assert length(Game.state("game").players) == 4
+  end
 
-      assert Timex.is_valid?(Game.state("game_9").time_of_round)
-    end
+  test "should run given actions", %{pid: pid} do
+    Game.shift_automatically("game")
 
-    test "doesn't add existing players when adding from a list", %{player_attrs: player1} do
-      {:ok, _} = GameSupervisor.start_child("game_9")
-      players = [player1, build(:player), build(:player)]
+    # This way we can test adding by a list and we have the IDS we need.
+    p = build(:player)
+    pp = build(:player)
+    players = [p, pp]
 
-      # We'll use an existing game in this case
-      {:ok, :added} = Game.add_player("game_9", player1)
+    # Manually send the signal
+    Game.add_player("game", players)
 
-      # We just add the whole list since so we don't loop through all of them,
-      # otherwise we could have sticked with the previous solution of looping
-      # through each player's attributes.
-      {:ok, :added} = Game.add_player("game_9", players)
+    players = Game.state("game").players
 
-      # We should only have 3 players since id 1 was already added
-      assert length(Game.state("game_9").players) == 3
-    end
+    actions = [
+      %{"from" => p.id, "to" => pp.id, "type" => "damage", "amount" => 4},
+      %{"from" => pp.id, "to" => p.id, "type" => "damage", "amount" => 12}
+    ]
 
-    test "should run given actions" do
-      {:ok, pid} = GameSupervisor.start_child("game_10")
-      Game.shift_automatically("game_10")
+    Game.add_action("game", actions)
 
-      # This way we can test adding by a list and we have the IDS we need.
-      p = build(:player)
-      pp = build(:player)
-      players = [p, pp]
+    Process.send(pid, :round_timeout, [:nosuspend])
 
-      # Manually send the signal
-      Game.add_player("game_10", players)
+    updated_players = Game.state("game").players
 
-      players = Game.state("game_10").players
+    # Players state should have changed.
+    refute updated_players == players
+    assert length(Game.state("game").players) == 2
 
-      actions = [
-        %{"from" => p.id, "to" => pp.id, "type" => "damage", "amount" => 4},
-        %{"from" => pp.id, "to" => p.id, "type" => "damage", "amount" => 12}
-      ]
+    hps = Enum.map(updated_players, & &1.health_points)
 
-      Game.add_action("game_10", actions)
+    assert 46 in hps && 38 in hps
+  end
 
-      Process.send(pid, :round_timeout, [:nosuspend])
+  test "remove dead players", %{pid: pid} do
+    Game.shift_automatically("game")
+    player = build(:player)
+    player_2 = build(:player)
 
-      updated_players = Game.state("game_10").players
+    Game.add_player("game", [player, player_2])
 
-      # Players state should have changed.
-      refute updated_players == players
-      assert length(Game.state("game_10").players) == 2
+    assert length(Game.state("game").players) == 2
 
-      hps = Enum.map(updated_players, & &1.health_points)
+    # Player 1 kills player 2
+    action = [%{"from" => player.id, "type" => "damage", "amount" => 60}]
 
-      assert 46 in hps && 38 in hps
-    end
+    Game.add_action("game", action)
 
-    test "remove dead players" do
-      {:ok, pid} = GameSupervisor.start_child("game_11")
-      Game.shift_automatically("game_11")
-      player = build(:player)
-      player_2 = build(:player)
+    # Manually running actions
+    Process.send(pid, :round_timeout, [:nosuspend])
+    Process.send(pid, :standby_phase, [:nosuspend])
 
-      Game.add_player("game_11", [player, player_2])
+    assert length(Game.state("game").players) == 1
+  end
 
-      assert length(Game.state("game_11").players) == 2
+  test "allows only 1000 players per game" do
+    Game.shift_automatically("game")
 
-      # Player 1 kills player 2
-      action = [%{"from" => player.id, "type" => "damage", "amount" => 60}]
+    Enum.each(1..1_000, fn _ -> Game.add_player("game", build(:player)) end)
 
-      Game.add_action("game_11", action)
+    assert {:error, :max_length} = Game.add_player("game", build(:player))
+  end
 
-      # Manually running actions
-      Process.send(pid, :round_timeout, [:nosuspend])
-      Process.send(pid, :standby_phase, [:nosuspend])
+  test "shields should be applied first and damage reduced", %{pid: pid} do
+    Game.shift_automatically("game")
+    p = build(:player)
+    pp = build(:player)
 
-      assert length(Game.state("game_11").players) == 1
-    end
+    Game.add_player("game", [p, pp])
 
-    test "allows only 1000 players per game" do
-      {:ok, _pid} = GameSupervisor.start_child("game_13")
-      Game.shift_automatically("game_13")
+    # 14% of 40 is 5.6, final damage to PP should be 34.4
+    actions = [
+      %{"from" => p.id, "type" => "damage", "amount" => 40},
+      %{"from" => pp.id, "type" => "shield", "amount" => 14}
+    ]
 
-      Enum.each(1..1_000, fn _ -> Game.add_player("game_13", build(:player)) end)
+    Game.add_action("game", actions)
 
-      assert {:error, :max_length} = Game.add_player("game_13", build(:player))
-    end
+    Process.send(pid, :round_timeout, [:nosuspend])
 
-    test "shields should be applied first and damage reduced" do
-      {:ok, pid} = GameSupervisor.start_child("game_14")
-      Game.shift_automatically("game_14")
-      p = build(:player)
-      pp = build(:player)
+    # There are no dead players at this stage
 
-      Game.add_player("game_14", [p, pp])
+    players = Game.state("game").players
+    hps = Enum.map(players, & &1.health_points)
 
-      # 14% of 40 is 5.6, final damage to PP should be 34.4
-      actions = [
-        %{"from" => p.id, "type" => "damage", "amount" => 40},
-        %{"from" => pp.id, "type" => "shield", "amount" => 14}
-      ]
+    # Hp of attacked user is 15.6, we're rounding to the nearest integer = 16
+    assert 16 in hps
+  end
 
-      Game.add_action("game_14", actions)
+  test "removes shields from every player alive", %{pid: pid} do
+    Game.shift_automatically("game")
+    p = build(:player)
+    pp = build(:player)
+    ppp = build(:player)
 
-      Process.send(pid, :round_timeout, [:nosuspend])
+    get_shields = fn -> Enum.map(Game.state("game").players, & &1.shield_points) end
 
-      # There are no dead players at this stage
+    Game.add_player("game", [p, pp, ppp])
 
-      players = Game.state("game_14").players
-      hps = Enum.map(players, & &1.health_points)
+    actions = [
+      %{"from" => p.id, "type" => "shield", "amount" => 16},
+      %{"from" => pp.id, "type" => "shield", "amount" => 18},
+      %{"from" => ppp.id, "type" => "shield", "amount" => 31}
+    ]
 
-      # Hp of attacked user is 15.6, we're rounding to the nearest integer = 16
-      assert 16 in hps
-    end
+    Game.add_action("game", actions)
 
-    test "removes shields from every player alive" do
-      {:ok, pid} = GameSupervisor.start_child("game_15")
-      Game.shift_automatically("game_15")
-      p = build(:player)
-      pp = build(:player)
-      ppp = build(:player)
+    Process.send(pid, :round_timeout, [:nosuspend])
 
-      get_shields = fn -> Enum.map(Game.state("game_15").players, & &1.shield_points) end
+    assert get_shields.() == [16, 18, 31]
 
-      Game.add_player("game_15", [p, pp, ppp])
+    Process.send(pid, :standby_phase, [:nosuspend])
 
-      actions = [
-        %{"from" => p.id, "type" => "shield", "amount" => 16},
-        %{"from" => pp.id, "type" => "shield", "amount" => 18},
-        %{"from" => ppp.id, "type" => "shield", "amount" => 31}
-      ]
+    # Shields should be removed at this point
+    assert get_shields.() == [0, 0, 0]
+  end
 
-      Game.add_action("game_15", actions)
+  test "should remove actions after the round", %{pid: pid} do
+    Game.shift_automatically("game")
+    p = build(:player)
+    pp = build(:player)
+    ppp = build(:player)
 
-      Process.send(pid, :round_timeout, [:nosuspend])
+    Game.add_player("game", [p, pp, ppp])
 
-      assert get_shields.() == [16, 18, 31]
+    actions = [
+      %{"from" => p.id, "type" => "shield", "amount" => 16},
+      %{"from" => pp.id, "type" => "shield", "amount" => 18},
+      %{"from" => ppp.id, "type" => "shield", "amount" => 31}
+    ]
 
-      Process.send(pid, :standby_phase, [:nosuspend])
+    Game.add_action("game", actions)
 
-      # Shields should be removed at this point
-      assert get_shields.() == [0, 0, 0]
-    end
+    game_state = fn -> Game.state("game") end
 
-    test "should remove actions after the round" do
-      {:ok, pid} = GameSupervisor.start_child("game_16")
-      Game.shift_automatically("game_16")
-      p = build(:player)
-      pp = build(:player)
-      ppp = build(:player)
+    assert length(game_state.().actions) == 3
 
-      Game.add_player("game_16", [p, pp, ppp])
+    Process.send(pid, :round_timeout, [:nosuspend])
+    Process.send(pid, :standby_phase, [:nosuspend])
 
-      actions = [
-        %{"from" => p.id, "type" => "shield", "amount" => 16},
-        %{"from" => pp.id, "type" => "shield", "amount" => 18},
-        %{"from" => ppp.id, "type" => "shield", "amount" => 31}
-      ]
+    assert length(game_state.().actions) == 0
+  end
 
-      Game.add_action("game_16", actions)
+  test "should not add actions if the user isn't in the game" do
+    Game.shift_automatically("game")
 
-      game_state = fn -> Game.state("game_16") end
+    action = [%{"from" => 123, "type" => "shield", "amount" => 16}]
 
-      assert length(game_state.().actions) == 3
+    Game.add_action("game", action)
 
-      Process.send(pid, :round_timeout, [:nosuspend])
-      Process.send(pid, :standby_phase, [:nosuspend])
+    assert length(Game.state("game").actions) == 0
+  end
 
-      assert length(game_state.().actions) == 0
-    end
+  test "convert all players to struct when adding from a list" do
+    Game.shift_automatically("game")
 
-    test "should not add actions if the user isn't in the game" do
-      {:ok, _pid} = GameSupervisor.start_child("game_17")
-      Game.shift_automatically("game_17")
+    players = [
+      %{id: 12, inventory: build(:inventory)},
+      %{id: 151, inventory: build(:inventory)},
+      %{id: 12, inventory: build(:inventory)}
+    ]
 
-      action = [%{"from" => 123, "type" => "shield", "amount" => 16}]
+    Game.add_player("game", players)
 
-      Game.add_action("game_17", action)
+    state = Game.state("game")
 
-      assert length(Game.state("game_17").actions) == 0
-    end
+    assert Enum.all?(state.players, &Map.has_key?(&1, :__struct__))
+  end
 
-    test "convert all players to struct when adding from a list" do
-      {:ok, _pid} = GameSupervisor.start_child("game_18")
+  test "should create the user inventory when adding the player to a game" do
+    Game.shift_automatically("game")
 
-      Game.shift_automatically("game_18")
+    player = %{id: 123, inventory: %{item_id: 51, amount: 4}}
 
-      players = [
-        %{id: 12},
-        %{id: 151},
-        %{id: 12}
-      ]
+    Game.add_player("game", player)
 
-      Game.add_player("game_18", players)
+    state = Game.state("game")
 
-      state = Game.state("game_18")
+    assert Enum.all?(state.players, &Map.has_key?(&1, :__struct__))
+    assert Enum.at(state.players, 0).inventory == [%Inventory{item_id: 51, amount: 4}]
+  end
 
-      assert Enum.all?(state.players, &Map.has_key?(&1, :__struct__))
-    end
+  test "should create inventory struct when adding players from list" do
+    players = [
+      %{id: 12, inventory: %{item_id: 50, amount: 1}},
+      %{id: 141, inventory: %{item_id: 40, amount: 12}}
+    ]
 
-    @tag :only
-    test "should create the user inventory when adding the player to a game" do
-      {:ok, _} = GameSupervisor.start_child("game_19")
+    Game.add_player("game", players)
 
-      Game.shift_automatically("game_19")
+    state = Game.state("game")
 
-      player = %{id: 123, inventory: %{item_id: 51, amount: 4}}
+    expected = [
+      [%Inventory{item_id: 50, amount: 1}],
+      [%Inventory{item_id: 40, amount: 12}]
+    ]
 
-      Game.add_player("game_19", player)
-
-      state = Game.state("game_19")
-
-      assert Enum.all?(state.players, &Map.has_key?(&1, :__struct__))
-      assert Enum.at(state.players, 0).inventory == %Inventory{item_id: 51, amount: 4}
-    end
-
-    @tag :only
-    test "should create inventory struct when adding players from list" do
-      players = [
-        %{id: 12, inventory: %{item_id: 50, amount: 1}},
-        %{id: 141, inventory: %{item_id: 40, amount: 12}}
-      ]
-
-      Game.add_player("game_19", players)
-
-      state = Game.state("game_19")
-
-      expected = [
-        %Inventory{item_id: 51, amount: 4},
-        %Inventory{item_id: 50, amount: 1},
-        %Inventory{item_id: 40, amount: 12}
-      ]
-
-      assert Enum.map(state.players, & &1.inventory) == expected
-    end
+    assert Enum.map(state.players, & &1.inventory) == expected
   end
 end
