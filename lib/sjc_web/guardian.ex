f0832da367f2cc08c692b615e91fe992d39fc0cd
Miguel Diaz <diazmiiguel@gmail.com>
2018-11-27 21:00:57 -0400

    checks for resource before creating jwt

--- lib/sjc_web/guardian.ex
+++ lib/sjc_web/guardian.ex
@@ -6,25 +6,29 @@ defmodule SjcWeb.Guardian do
   alias Sjc.Repo
   alias Sjc.Models.User
 
-  def subject_for_token(%User{} = resource, _claims) do
-    sub = to_string(resource.id)
-    {:ok, sub}
-  end
-
   def subject_for_token(nil, _claims) do
     {:error, :missing_resource}
+  end
+
+  def subject_for_token(%{id: id}, _claims) do
+    case Repo.get(User, id) do
+      nil -> {:error, :resource_not_found}
+      %User{} = _user -> {:ok, to_string(id)}
+    end
   end
 
   def subject_for_token(_res, _claims) do
     {:error, :invalid_resource}
   end
 
-  def resource_from_claims(claims) do
-    id = claims["sub"]
+  def resource_from_claims(%{"sub" => sub}) do
+    case Repo.get(User, sub) do
+      nil ->
+        {:error, :not_found}
 
-    case Repo.get(User, id) do
-      nil -> {:error, :not_found}
-      user -> {:ok, user}
+      user ->
+        IO.inspect(user)
+        {:ok, user}
     end
   end
 end
